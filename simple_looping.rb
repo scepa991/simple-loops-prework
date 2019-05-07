127bd668e91a5444c51e29bd6d35103e86f3d476
Stefan Scepanovic <scepa91@icloud.com>
2019-05-07 18:28:49 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,7 +1,7 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-  7.times do 
+  7.times do
     puts "Welcome to Flatiron School's Web Development Course!"
 end
 
